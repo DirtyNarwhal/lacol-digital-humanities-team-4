e8c7e1b05bb28d806ebd58e80896f5ceaaaee392
Marvin Lopez Acevedo <81580153+DirtyNarwhal@users.noreply.github.com>
2022-07-26 12:09:04 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/u/2/d/1hbkqSzyTCvkgP-GPbIcCsiRheMkdJ2qY71gdvmx5czw/edit?usp=drive_web&ouid=108839439327753708794';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wYf751VhZku3704UvM-AlxhSQjDm4rWULIb6sRYgCnA/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCFfVN7LyoJOOQn75_uSxUSAWf9sxeCOl8';
